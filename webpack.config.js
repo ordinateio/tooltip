05f1c5c46bcb933bf8d34cf7843d625076f7486e
Callisto <callisto2410@yandex.ru>
2021-01-14 18:11:01 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
 const Path = require("path");
+// noinspection NpmUsedModulesInstalled
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
+const WP = {
+    mode: "production",
+    devtool: "source-map",
     performance: {
-        hints: false
+        hints: false,
     },
     optimization: {
         minimize: true,
         minimizer: [
             new TerserPlugin({
-                terserOptions: {
-                    format: {
-                        comments: false,
-                    },
-                },
                 extractComments: false,
             }),
         ],
@@ -23,46 +21,52 @@ module.exports = {
         filename: "bundle.js",
         path: Path.resolve(__dirname, "demo", "dist"),
     },
-    devtool: "source-map",
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: false,
-                            url: false,
-                        }
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    ["autoprefixer"],
-                                ],
-                            },
-                        },
-                    },
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.tsx?$/,
-                use: [{
-                    loader: "ts-loader",
-                    options: {
-                        configFile: "tsconfig.loader.json"
-                    }
-                }],
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
+    cache: {
+        type: "filesystem",
+        cacheDirectory: Path.resolve(__dirname, ".cache"),
     },
 };
+
+WP.module = {
+    rules: [
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                "style-loader",
+                {
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: false,
+                        url: false,
+                    }
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            plugins: [
+                                ["autoprefixer"],
+                            ],
+                        },
+                    },
+                },
+                "sass-loader",
+            ],
+        },
+        {
+            test: /\.ts$/,
+            use: [{
+                loader: "ts-loader",
+                options: {
+                    configFile: "tsconfig.loader.json",
+                }
+            }],
+        },
+    ],
+}
+
+WP.resolve = {
+    extensions: [".ts", ".js", ".scss"],
+}
+
+module.exports = WP;
