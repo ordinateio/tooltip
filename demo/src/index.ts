28226db8d8034d1ba4d1bc90edeb2a27b5b5c8af
Ordinate IO <ordinateio@gmail.com>
2021-09-01 16:22:17 +0900

    Completely rework

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,54 +1,167 @@
-import '../../src/index.scss';
-import {Tooltip, TooltipInstance} from '../../src';
-
 import './index.scss';
 
-const tooltip = new Tooltip();
-tooltip.lexicon.extend({
-    'test': {
-        en: 'Lexicon!',
-        ru: 'Лексикон!',
-    }
-});
+import '../../src/index.scss';
+import {Tooltip} from '../../src';
 
-/* Nav. */
-tooltip.set('.nav-container', {
+let tooltip = new Tooltip();
+
+/* Nav */
+tooltip.create('.nav-container', {
     trigger: 'click',
     target: '.nav-container__button',
-    onShow(instance: TooltipInstance) {
+    content: 'attribute:data-tooltip-content',
+    classes: 'nav-container__tooltip-nav',
+    theme: 'light',
+    placement: 'bottom',
+    interactive: true,
+    animation: 'shift-away',
+    onShow(instance) {
         instance.reference.classList.add('nav-container__button--active');
     },
-    onHide(instance: TooltipInstance) {
+    onHide(instance) {
         instance.reference.classList.remove('nav-container__button--active');
     },
 });
 
-/* Placements. */
-tooltip.set('.placement', {
+/* Placement top */
+tooltip.create('.placement', {
     trigger: 'mouseenter',
-    target: '.placement__item',
+    target: '.placement__item--top-start',
+    content: 'attribute:data-tooltip-content',
+    placement: 'top-start',
 });
 
-/* Other. */
-tooltip.set('.other', {
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--top',
+    content: 'attribute:data-tooltip-content',
+    placement: 'top',
+});
+
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--top-end',
+    content: 'attribute:data-tooltip-content',
+    placement: 'top-end',
+});
+
+/* Placement right */
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--right-start',
+    content: 'attribute:data-tooltip-content',
+    placement: 'right-start',
+});
+
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--right',
+    content: 'attribute:data-tooltip-content',
+    placement: 'right',
+});
+
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--right-end',
+    content: 'attribute:data-tooltip-content',
+    placement: 'right-end',
+});
+
+/* Placement bottom */
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--bottom-start',
+    content: 'attribute:data-tooltip-content',
+    placement: 'bottom-start',
+});
+
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--bottom',
+    content: 'attribute:data-tooltip-content',
+    placement: 'bottom',
+});
+
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--bottom-end',
+    content: 'attribute:data-tooltip-content',
+    placement: 'bottom-end',
+});
+
+/* Placement left */
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--left-start',
+    content: 'attribute:data-tooltip-content',
+    placement: 'left-start',
+});
+
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--left',
+    content: 'attribute:data-tooltip-content',
+    placement: 'left',
+});
+
+tooltip.create('.placement', {
+    trigger: 'mouseenter',
+    target: '.placement__item--left-end',
+    content: 'attribute:data-tooltip-content',
+    placement: 'left-end',
+});
+
+/* Click */
+let clickTooltip = tooltip.create('.events', {
     trigger: 'click',
-    target: '.other__item--click',
+    target: '.events__item--click',
+    content: 'attribute:data-tooltip-content',
+    interactive: true,
 });
 
-tooltip.set('.other', {
+/* Mouseenter */
+let mouseenterTooltip = tooltip.create('.events', {
     trigger: 'mouseenter',
-    target: '.other__item--mouseenter',
+    target: '.events__item--mouseenter',
+    content: 'attribute:data-tooltip-content',
 });
 
-tooltip.set('.other', {
+/* Focus */
+tooltip.create('.events', {
     trigger: 'focus',
-    target: '.other__item--focus input',
+    target: '.events__item--focus input',
+    content: 'attribute:data-tooltip-content',
 });
 
-tooltip.set('.other', {
-    trigger: 'click',
-    target: '.other__item--toggle-lang',
-    onShow(instance: TooltipInstance) {
-        tooltip.lexicon.locale = instance.reference.getAttribute('data-value') ?? 'en';
-    },
+/* Follow */
+tooltip.create('.events', {
+    trigger: 'mouseenter',
+    target: '.events__item--follow',
+    content: 'attribute:data-tooltip-content',
+    theme: 'light',
+    followCursor: true,
+});
+
+/* Destroy click */
+let destroyClick = document.querySelector('.methods__item--destroy-click') as HTMLElement | null;
+if (!destroyClick) throw new Error('Element ".methods__item--destroy-click" is null.');
+
+destroyClick.addEventListener('click', () => {
+    for (let item of clickTooltip) item.destroy();
+});
+
+/* Disable mouseenter */
+let disableMouseenter = document.querySelector('.methods__item--disable-mouseenter') as HTMLElement | null;
+if (!disableMouseenter) throw new Error('Element ".methods__item--disable-mouseenter" is null.');
+
+disableMouseenter.addEventListener('click', () => {
+    for (let item of mouseenterTooltip) item.disable();
+});
+
+/* Enable mouseenter */
+let enableMouseenter = document.querySelector('.methods__item--enable-mouseenter') as HTMLElement | null;
+if (!enableMouseenter) throw new Error('Element ".methods__item--enable-mouseenter" is null.');
+
+enableMouseenter.addEventListener('click', () => {
+    for (let item of mouseenterTooltip) item.enable();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,62 @@
-import {Lexicon} from '@ordinateio/lexicon';
-import Tippy, {delegate, followCursor, Instance as TippyInstance, ReferenceElement} from 'tippy.js';
+import Tippy, {delegate, followCursor, Instance, ReferenceElement} from 'tippy.js';
+
+export interface TooltipProperties {
+    /**
+     * Event to display the tooltip.
+     */
+    trigger: 'mouseenter' | 'focus' | 'mouseenter focus' | 'focusin' | 'click' | 'manual';
+
+    /**
+     * Target item.
+     */
+    target: string;
+
+    /**
+     * Tooltip content.
+     */
+    content: string;
+
+    /**
+     * Additional CSS classes separated by a space.
+     */
+    classes?: string;
+
+    /**
+     * Theme for tooltip.
+     */
+    theme?: 'dark' | 'light';
+
+    /**
+     * Position of the tooltip.
+     */
+    placement?: | 'top' | 'top-start' | 'top-end'
+        | 'right' | 'right-start' | 'right-end'
+        | 'bottom' | 'bottom-start' | 'bottom-end'
+        | 'left' | 'left-start' | 'left-end'
+        | 'auto' | 'auto-start' | 'auto-end';
+
+    /**
+     * Tooltip animation.
+     */
+    animation?: 'scale' | 'shift-away' | 'shift-toward' | 'perspective';
+
+    /**
+     * If 'true' interactive mode will be enabled.
+     */
+    interactive?: boolean;
+
+    /**
+     * If 'true' will enable cursor following.
+     */
+    followCursor?: boolean;
+
+    /**
+     * Determines if content strings are parsed as HTML instead of text.
+     */
+    allowHTML?: boolean;
+}
 
 export interface TooltipCallbacks {
-    /**
-     * Lifecycle hook invoked before the tooltip properties have been updated.
-     *
-     * @param instance
-     * @param properties
-     */
-    onBeforeUpdate?(instance: TooltipInstance, properties: Partial<TooltipCallbacks>): void;
-
-    /**
-     * Lifecycle hook invoked after the tooltip properties have been updated.
-     *
-     * @param instance
-     * @param properties
-     */
-    onAfterUpdate?(instance: TooltipInstance, properties: Partial<TooltipCallbacks>): void;
-
     /**
      * Lifecycle hook invoked when the tooltip has been created.
      *
@@ -40,160 +79,53 @@ export interface TooltipCallbacks {
     onHide?(instance: TooltipInstance): void;
 
     /**
-     * Lifecycle hook invoked when the tooltip has fully transitioned out and is unmounted from the DOM.
-     *
-     * @param instance
-     */
-    onHidden?(instance: TooltipInstance): void;
-
-    /**
      * Lifecycle hook invoked when the tooltip begins to transition in.
      *
      * @param instance
      */
     onShow?(instance: TooltipInstance): void;
-
-    /**
-     * Lifecycle hook invoked when the tooltip has fully transitioned in.
-     *
-     * @param instance
-     */
-    onShown?(instance: TooltipInstance): void;
-
-    /**
-     * Lifecycle hook invoked when the tooltip has been mounted to the DOM.
-     *
-     * @param instance
-     */
-    onMount?(instance: TooltipInstance): void;
 }
 
-export interface TooltipProperties {
-    /**
-     * An instance of the Lexicon.
-     */
-    lexicon: Lexicon;
-}
-
-export interface TooltipAttributes {
-    /**
-     * Tooltip content.
-     */
-    content: string;
-
-    /**
-     * Additional CSS classes separated by a space.
-     */
-    class: string;
-
-    /**
-     * Theme for tooltip.
-     */
-    theme: TooltipTheme;
-
-    /**
-     * Position of the tooltip.
-     */
-    placement: TooltipPlacement;
-
-    /**
-     * Tooltip animation.
-     */
-    animation: TooltipAnimation;
-
-    /**
-     * If 'true' interactive mode will be enabled.
-     */
-    interactive: boolean;
-
-    /**
-     * If 'true' will enable cursor following.
-     */
-    followCursor: boolean;
-}
-
-export interface TooltipPropertiesForSet extends TooltipCallbacks {
-    /**
-     * Event to display the tooltip.
-     */
-    trigger: TooltipTrigger;
-
-    /**
-     * Target item.
-     */
-    target: string;
-}
-
-export interface TooltipTarget extends ReferenceElement {
-    dataset?: DOMStringMap;
-}
-
-export interface TooltipInstance extends TippyInstance {
+export interface TooltipInstance extends Instance {
     reference: TooltipTarget;
+
+    /**
+     * Destroy the tooltip.
+     */
+    destroy(): void;
+
+    /**
+     * Disable the tooltip.
+     */
+    disable(): void;
+
+    /**
+     * Enable the tooltip.
+     */
+    enable(): void;
 }
 
-export type TooltipTheme = 'dark' | 'light';
+export type TooltipTarget = ReferenceElement;
 
-export type TooltipPlacement =
-    | 'top' | 'top-start' | 'top-end'
-    | 'right' | 'right-start' | 'right-end'
-    | 'bottom' | 'bottom-start' | 'bottom-end'
-    | 'left' | 'left-start' | 'left-end'
-    | 'auto' | 'auto-start' | 'auto-end';
-
-export type TooltipAnimation = 'scale' | 'shift-away' | 'shift-toward' | 'perspective';
-
-export type TooltipTrigger = 'mouseenter' | 'focus' | 'mouseenter focus' | 'focusin' | 'click' | 'manual';
-
-export type TooltipOnShow = ((instance: TooltipInstance) => void) | undefined;
-
-/**
- * class Tooltip - The implementation of simple tooltips.
- *
- * [Github]{@link https://github.com/ordinateio/tooltip}
- */
 export class Tooltip {
-    private readonly properties: TooltipProperties = {
-        lexicon: new Lexicon(),
-    };
-
-    /**
-     * An instance of the Lexicon.
-     */
-    get lexicon(): Lexicon {
-        return this.properties.lexicon;
-    }
-
-    /**
-     * An instance of the Lexicon.
-     *
-     * @param lexicon An instance of the Lexicon.
-     */
-    set lexicon(lexicon: Lexicon) {
-        this.properties.lexicon = lexicon;
-    }
-
     /**
      * Tooltip constructor.
+     *
+     * @param properties
      */
     constructor(properties: Partial<TooltipProperties> = {}) {
-        this.properties = {
-            ...this.properties,
-            ...properties,
-        };
-
-
         Tippy.setDefaultProps({
             animation: 'scale',
             allowHTML: true,
             inertia: true,
             arrow: true,
             ignoreAttributes: true,
-            zIndex: 99_999,
+            zIndex: 100,
             maxWidth: 300,
             theme: 'dark',
             offset: [0, 6],
-            plugins: [followCursor]
+            plugins: [followCursor],
+            ...properties
         });
     }
 
@@ -203,164 +135,73 @@ export class Tooltip {
      * @param selector A container selector for setting a delegate.
      * @param properties
      */
-    set(selector: string, properties: TooltipPropertiesForSet): void {
-        delegate(selector, {
+    create(selector: string, properties: TooltipProperties & Partial<TooltipCallbacks>): TooltipInstance[] {
+        return delegate(selector, {
             ...properties,
-            onShow: this.setProperties.bind(this, properties.onShow),
+            onShow: Tooltip.onShowHandler.bind(this, properties),
             touch: (properties.trigger === 'click') ? 'hold' : true,
         });
     }
 
     /**
-     * Returns an existing instance, otherwise returns undefined.
+     * Callback handler for onShow method.
      *
-     * @param selector
-     */
-    getInstance(selector: string): TooltipInstance | undefined {
-        const element = document.querySelector(selector) as TooltipTarget | null;
-
-        return element ? element._tippy : undefined;
-    }
-
-    /**
-     * Destroy the tooltip.
-     *
-     * @param selector
-     */
-    destroy(selector: string): void {
-        const instance = this.getInstance(selector);
-
-        instance && instance.destroy();
-    }
-
-    /**
-     * Disable the tooltip.
-     *
-     * @param selector
-     */
-    disable(selector: string): void {
-        const instance = this.getInstance(selector);
-
-        instance && instance.disable();
-    }
-
-    /**
-     * Enable the tooltip.
-     *
-     * @param selector
-     */
-    enable(selector: string): void {
-        const instance = this.getInstance(selector);
-
-        instance && instance.enable();
-    }
-
-    /**
-     * Set the content for the tooltip.
-     *
-     * @param selector
-     * @param content
-     */
-    setContent(selector: string, content: string): void {
-        const instance = this.getInstance(selector);
-
-        instance && instance.setContent(content);
-    }
-
-    /**
-     * Hide the tooltip.
-     *
-     * @param selector
-     */
-    hide(selector: string): void {
-        const instance = this.getInstance(selector);
-
-        instance && instance.hide();
-    }
-
-    /**
-     * Show the tooltip.
-     *
-     * @param selector
-     */
-    show(selector: string): void {
-        const instance = this.getInstance(selector);
-
-        instance && instance.show();
-    }
-
-    /**
-     * Unmount the tooltip from the DOM.
-     *
-     * @param selector
-     */
-    unmount(selector: string): void {
-        const instance = this.getInstance(selector);
-
-        instance && instance.unmount();
-    }
-
-    /**
-     * Sets properties for an instance.
-     *
-     * @param onShow
+     * @param properties
      * @param instance
      * @private
      */
-    private setProperties(onShow: TooltipOnShow, instance: TooltipInstance): void {
-        onShow && onShow(instance);
+    private static onShowHandler(properties: TooltipProperties & Partial<TooltipCallbacks>, instance: TooltipInstance): void {
+        properties.onShow && properties.onShow(instance);
 
-        const properties = this.getAttributes(instance.reference);
-        const popper = instance.popper;
-
-        popper.classList.add('tooltip-root');
-        popper.querySelector('.tippy-box')?.classList.add('tooltip-container');
-        popper.querySelector('.tippy-arrow')?.classList.add('tooltip-container__arrow');
-        popper.querySelector('.tippy-content')?.classList.add('tooltip-container__content');
-        properties.class && popper.classList.add(...properties.class.split(' '));
+        Tooltip.setContentFromAttribute(properties, instance);
+        Tooltip.setContentFromSelector(properties);
+        Tooltip.setClasses(properties, instance);
 
         instance.setProps(properties);
     }
 
     /**
-     * Returns the property obtained from the target element.
+     * Sets the content of the tooltip from the attribute of the target element.
      *
-     * @param target Target item.
+     * @param properties
+     * @param instance
      * @private
      */
-    private getAttributes(target: TooltipTarget): TooltipAttributes {
-        const data = Tooltip.getRawAttributes(target);
-
-        if (data.content.startsWith('selector:')) {
-            const selector = data.content.replace('selector:', '').trim();
-            data.content = document.querySelector(selector)?.outerHTML ?? data.content;
+    private static setContentFromAttribute(properties: TooltipProperties, instance: TooltipInstance) {
+        if (properties.content.startsWith('attribute:')) {
+            let selector = properties.content.replace('attribute:', '').trim();
+            properties.content = instance.reference.getAttribute(selector) ?? properties.content;
         }
-
-        if (data.content.startsWith('lexicon:')) {
-            const lexicon = data.content.replace('lexicon:', '').trim();
-            data.content = this.properties.lexicon.get(lexicon);
-        }
-
-        return data;
     }
 
     /**
-     * Returns the properties derived from the attributes.
+     * Sets the content of the tooltip from the specified selector.
      *
-     * @param target Target item.
+     * @param properties
      * @private
      */
-    private static getRawAttributes(target: TooltipTarget): TooltipAttributes {
-        const dataset = target.dataset ?? {};
+    private static setContentFromSelector(properties: TooltipProperties) {
+        if (properties.content.startsWith('selector:')) {
+            let selector = properties.content.replace('selector:', '').trim();
+            properties.content = document.querySelector(selector)?.outerHTML ?? properties.content;
+        }
+    }
 
-        return {
-            content: dataset.tooltipContent ?? 'Content is missing!',
-            theme: (dataset.tooltipTheme ?? 'dark') as TooltipTheme,
-            placement: (dataset.tooltipPlacement ?? 'top') as TooltipPlacement,
-            animation: (dataset.tooltipAnimation ?? 'scale') as TooltipAnimation,
-            interactive: dataset.tooltipInteractive === 'true',
-            followCursor: dataset.tooltipFollowCursor === 'true',
-            class: dataset.tooltipClass ?? '',
-        };
+    /**
+     * Sets custom classes for tooltips.
+     *
+     * @param properties
+     * @param instance
+     * @private
+     */
+    private static setClasses(properties: TooltipProperties, instance: TooltipInstance) {
+        let popper = instance.popper;
+
+        popper.classList.add('tooltip-root');
+        popper.querySelector('.tippy-box')?.classList.add('tooltip-container');
+        popper.querySelector('.tippy-arrow')?.classList.add('tooltip-container__arrow');
+        popper.querySelector('.tippy-content')?.classList.add('tooltip-container__content');
+
+        properties.classes && popper.classList.add(...properties.classes.split(' '));
     }
 }
